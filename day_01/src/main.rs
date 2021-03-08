6ba59ebfb5f1963d8468ff5b0aa0c5306acdf082
Bastian Gruber <foreach@me.com>
2021-03-08 19:09:06 +0100

    Added second part, updated code

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -4,12 +4,53 @@ fn main() {
     let contents = fs::read_to_string("puzzle_input.txt")
         .expect("Something went wrong reading the file");
 
-    let contents: Vec<u32> = contents.lines().filter_map(|s| Some(s.parse::<u32>().expect("not a number"))).collect();
+    let mut contents: Vec<usize> = contents.lines().filter_map(|s| Some(s.parse::<usize>().expect("not a number"))).collect();
+    contents.sort();
 
-    for (i, number) in contents.iter().enumerate() {
-       let mut sum = 0;
+    let (first, second) = find_two(&contents, 2020);
 
+    println!("---------FIND TWO-----------");
+    println!("First: {}, Second: {}, Sum: {}", first, second, first * second);
+    println!("----------------------------");
 
+    let (first, second, third) = find_three(&contents, 2020); 
+    
+    println!("---------FIND THREE-----------");
+    println!("First: {}, Second: {}, Third: {}, Sum: {}", first, second, third, first * second * third);
+    println!("----------------------------");
+}
 
+fn find_two(list: &Vec<usize>, sum: usize) -> (usize, usize) {
+    let mut l = 0;
+    let mut r = list.len() -1;
+    while l < r {
+        if list[l] + list[r] == sum {
+            return (list[l], list[r]);      
+        } else if list[l] + list[r] < sum {
+            l += 1;
+        } else {
+            r -= 1;
+        }
     }
+
+    return (0, 0);
+}
+
+fn find_three(list: &Vec<usize>, sum: usize) -> (usize, usize, usize) {
+    for (i, _) in list.iter().enumerate() {
+        let mut l = i + 1;
+        let mut r = list.len() - 1;
+
+        while l < r {
+            if list[i] + list[l] + list[r] == sum {
+                return (list[i], list[l], list[r]);
+            } else if list[i] + list[l] + list[r] < sum {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+    }
+
+    (0,0,0)
 }
